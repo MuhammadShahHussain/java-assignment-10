b4cd5df5ed596277b84f39f97d1d7a0aa15de47b
MuhammadShahHussain <128414274+MuhammadShahHussain@users.noreply.github.com>
2025-12-26 21:56:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// 1. Display first alert message
+alert("Error! Please enter a valid password.");
+
+// 2. Display second alert message
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// 3. Generate message through browser's developer console
+console.log("Hello... I can run JS through my web browser's console");
+
+// (Optional) You can test alerts by placing <script> in different sections of your HTML file:
+// - Inside <head>
+// - Inside <body> before HTML content
+// - Inside <body> after HTML content
